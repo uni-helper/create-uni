b9accad4b09f1996ff161232f03268c3b1846568
FliPPeDround <734243792@qq.com>
2023-07-04 16:42:13 +0800

    feat:  vitesse callBack

--- src/question/template/templateDate.ts
+++ src/question/template/templateDate.ts
@@ -1,4 +1,4 @@
-import { rmSync } from 'node:fs'
+import { readFileSync, rmSync, writeFileSync } from 'node:fs'
 import { join } from 'node:path'
 import { bold, lightCyan } from 'kolorist'
 import type { TemplateList } from './type'
@@ -15,8 +15,17 @@ export const templateList: TemplateList[] = [
         // 判断当前系统
         const isWin = process.platform === 'win32'
         // 如果非 Windows 系统，删除目录下的patches文件夹
-        if (!isWin)
-          rmSync(join(root, 'patches'), { recursive: true })
+        if (isWin)
+          return
+        rmSync(join(root, 'patches'), { recursive: true })
+
+        const pkgPath = join(root, 'package.json')
+        const packageJson = JSON.parse(readFileSync(pkgPath, 'utf8'))
+
+        // 删除 package.json 中的 pnpm
+        delete packageJson.pnpm
+
+        writeFileSync(pkgPath, JSON.stringify(packageJson, null, 2))
       },
     },
     description: `由${lightCyan('Uni Helper')}维护的快速启动模板`,
