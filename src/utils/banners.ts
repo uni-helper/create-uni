32cbc929d480452a9c09c321ddc59ab0dd55514b
flippedround <734243792@qq.com>
2023-07-16 11:12:28 +0800

    fix: not TTY Banner

--- src/utils/banners.ts
+++ src/utils/banners.ts
@@ -1,7 +1,9 @@
+import { green } from 'kolorist'
+
 /* eslint-disable no-console */
 export function printBanner() {
   const text = 'Uni-creator - 快速创建 uni-app 项目'
-  let output = ''
+  let colorText = ''
 
   const startColor = { r: 0x3B, g: 0xD1, b: 0x91 }
   const endColor = { r: 0x2B, g: 0x4C, b: 0xEE }
@@ -11,8 +13,13 @@ export function printBanner() {
     const red = Math.round(startColor.r + (endColor.r - startColor.r) * ratio)
     const green = Math.round(startColor.g + (endColor.g - startColor.g) * ratio)
     const blue = Math.round(startColor.b + (endColor.b - startColor.b) * ratio)
-    output += `\x1B[38;2;${red};${green};${blue}m${text[i]}\x1B[0m`
+    colorText += `\x1B[38;2;${red};${green};${blue}m${text[i]}\x1B[0m`
   }
+
+  const output = process.stdout.isTTY && process.stdout.getColorDepth() > 8
+    ? colorText
+    : green(text)
+
   console.log()
   console.log(output)
   console.log()
