3ab11e67f47e9bc79ddb38a3a1c9028191f43275
flippedround <734243792@qq.com>
2023-12-18 22:39:24 +0800

    feat: unocss tempate

---
+++ template/UI/unocss/src/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AppLogos from '@/components/AppLogos.vue'
+import InputEntry from '@/components/InputEntry.vue'
+import AppFooter from '@/components/AppFooter.vue'
+</script>
+
+<template>
+  <view px-10 py-20 text-center>
+    <AppLogos />
+    <InputEntry />
+    <AppFooter />
+  </view>
+</template>
