e3b46dfdb3fc4195e2b9c7f7b3ad92d3f6ad2e51
flippedround <734243792@qq.com>
2024-02-25 17:36:57 +0800

    feat: remove svg from template

--- template/base/src/components/AppLogos.vue
+++ template/base/src/components/AppLogos.vue
@@ -6,9 +6,9 @@
         uni-helper
       </text>
     </view>
-    <svg class="link-bar" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32">
-      <path fill="currentColor" d="M17 15V8h-2v7H8v2h7v7h2v-7h7v-2z" />
-    </svg>
+    <text class="link-bar">
+      +
+    </text>
     <view class="uni-helper-logo">
       <image class="uni-helper-logo__image" src="/static/vite.png" />
       <text class="uni-helper-logo__label purple">
@@ -49,10 +49,8 @@
   }
 }
 .link-bar {
-  font-size: 1.875rem;
-  line-height: 2.25rem;
   color: #9ca3af;
-  margin: auto 1rem;
+  margin: auto 1em;
   transition: all 500ms cubic-bezier(0.4, 0, 0.2, 1);
 }
 </style>
