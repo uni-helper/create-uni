41b36518e2eb0443847bbefa232353efa36a9d28
flippedround <734243792@qq.com>
2025-01-08 11:51:23 +0800

    feat(ui): update footer with interactive designer and license links

--- packages/ui/src/components/footer/index.tsx
+++ packages/ui/src/components/footer/index.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button'
+import { USER_EVENT } from '@/constants/USER_EVENT'
 import { Moon, Sun } from 'lucide-react'
 import { useEffect, useState } from 'preact/hooks'
 
@@ -22,8 +23,23 @@ export function Footer() {
     <footer className="mt-auto p-4 bg-zinc-100 dark:bg-zinc-800 text-zinc-600 dark:text-zinc-400 text-xs rounded-t-xl">
       <div className="flex justify-between items-center">
         <div class="italic">
-          <p>© 2025 Designed by FliPPeDround. </p>
-          <p className="text-zinc-400/70">MIT License</p>
+          <p>
+            © 2025 Designed by
+            {' '}
+            <span
+              className="hover:underline whitespace-pre cursor-pointer"
+              onClick={() => window.ipc.postMessage(`${USER_EVENT.OPEN}|https://github.com/FliPPeDround`)}
+            >
+              FliPPeDround
+            </span>
+            .
+          </p>
+          <span
+            className="text-zinc-400/70 hover:underline whitespace-pre cursor-pointer"
+            onClick={() => window.ipc.postMessage(`${USER_EVENT.OPEN}|https://github.com/uni-helper/create-uni`)}
+          >
+            MIT License
+          </span>
         </div>
 
         <Button
