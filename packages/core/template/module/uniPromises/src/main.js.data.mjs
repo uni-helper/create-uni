4c7c62d48bfb8a3a69d149b9e565365fa769aa76
Panpan <wurui.dev@gmail.com>
2025-11-26 16:09:14 +0800

    fix: remove core-js importer

--- packages/core/template/module/uniPromises/src/main.js.data.mjs
+++ packages/core/template/module/uniPromises/src/main.js.data.mjs
@@ -1,17 +1,13 @@
 export default function getData({ oldData }) {
   const uniPromisesEntry = {
     id: 'uniPromises',
-    importer: `import 'core-js/actual/array/iterator';
-import 'core-js/actual/promise';
-import 'core-js/actual/object/assign';
-import 'core-js/actual/promise/finally';`,
   }
   return {
     ...oldData,
     entries: oldData.entries.some(item => item.id === 'uniUse')
       ? oldData.entries
       : oldData.entries.flatMap(entry =>
-          entry.id === 'vue' ? [entry, uniPromisesEntry] : entry,
-        ),
+        entry.id === 'vue' ? [entry, uniPromisesEntry] : entry,
+      ),
   }
 }
--- packages/core/template/module/uniUse/src/main.js.data.mjs
+++ packages/core/template/module/uniUse/src/main.js.data.mjs
@@ -1,17 +1,13 @@
 export default function getData({ oldData }) {
   const uniUseEntry = {
     id: 'uniUse',
-    importer: `import 'core-js/actual/array/iterator';
-import 'core-js/actual/promise';
-import 'core-js/actual/object/assign';
-import 'core-js/actual/promise/finally';`,
   }
   return {
     ...oldData,
     entries: oldData.entries.some(item => item.id === 'uniPromises')
       ? oldData.entries
       : oldData.entries.flatMap(entry =>
-          entry.id === 'vue' ? [entry, uniUseEntry] : entry,
-        ),
+        entry.id === 'vue' ? [entry, uniUseEntry] : entry,
+      ),
   }
 }
