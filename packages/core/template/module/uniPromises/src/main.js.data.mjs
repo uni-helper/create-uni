56a3c38b7a58337889845f97d1e16b9c2790e8c0
Panpan <wurui.dev@gmail.com>
2025-11-27 11:57:46 +0800

    fix: remove core-js importer (#155)
    * fix: remove core-js importer
    
    * chore: lint fix

--- packages/core/template/module/uniPromises/src/main.js.data.mjs
+++ packages/core/template/module/uniPromises/src/main.js.data.mjs
@@ -1,10 +1,6 @@
 export default function getData({ oldData }) {
   const uniPromisesEntry = {
     id: 'uniPromises',
-    importer: `import 'core-js/actual/array/iterator';
-import 'core-js/actual/promise';
-import 'core-js/actual/object/assign';
-import 'core-js/actual/promise/finally';`,
   }
   return {
     ...oldData,
--- packages/core/template/module/uniUse/src/main.js.data.mjs
+++ packages/core/template/module/uniUse/src/main.js.data.mjs
@@ -1,10 +1,6 @@
 export default function getData({ oldData }) {
   const uniUseEntry = {
     id: 'uniUse',
-    importer: `import 'core-js/actual/array/iterator';
-import 'core-js/actual/promise';
-import 'core-js/actual/object/assign';
-import 'core-js/actual/promise/finally';`,
   }
   return {
     ...oldData,
